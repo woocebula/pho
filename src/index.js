6a5005f89b953133d9da225d4b9b721c580dcd13
woocebula <woocebula@gmail.com>
2021-03-07 17:05:48 +0100

    uporzadkowane komponenty

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import dude from './dude.jpg;';
+import dude from './dude.jpg';
+import damn from './damn.jpg';
 
 
 const Button = ({price, handleClick}) => <button onClick={handleClick}>
   Add</button>;
 const Button1 = ({price, handleClick}) => <button onClick={handleClick}>
-  Delete </button>
+  Delete </button>;
+
+
+
+const Header = ({menu}) => {
+  return (
+    <div>
+      <h1>{menu}</h1>
+    </div>
+  );
+};
+
+const Position = ({positions}) => {
+  return (
+    <li>
+     {positions.dish} {positions.price}
+    </li>
+      
+  ); 
+};
+
+const List = ({positions, rolls, pho, curry, setRolls, setPho, setCurry, totalQuanti}) => {
+  return (
+    <ul>
+        <Position positions={positions[0]}/><Button handleClick={() => setRolls(rolls + 1)}/>
+        {rolls > 0 
+          ? <Button1 handleClick={() => setRolls(rolls - 1)}/>
+          : <span></span>
+
+        }
+
+        <Position positions={positions[1]}/><Button handleClick={() => setPho(pho + 1)}/>
+        {pho > 0 && <Button1 handleClick={() => setPho(pho - 1)}/>}
+
+        <Position positions={positions[2]}/><Button handleClick={() => setCurry(curry + 1)}/>
+        {curry > 0 && <Button1 handleClick={() => setCurry(curry - 1)}/>}
+
+    </ul>
+  );
+};
 
 const Phorder = ({text, price, number}) => {
   
@@ -52,50 +92,13 @@ return (
   );
 };
 
-const Header = ({menu}) => {
-  return (
-    <div>
-      <h1>{menu}</h1>
-    </div>
-  );
-};
-
-const Position = ({positions}) => {
-  return (
-    <li>
-     {positions.dish} {positions.price}
-    </li>
-      
-  ); 
-};
-
-const List = ({positions, rolls, pho, curry, setRolls, setPho, setCurry, totalQuanti}) => {
-  return (
-    <ul>
-        <Position positions={positions[0]}/><Button handleClick={() => setRolls(rolls + 1)}/>
-        {rolls > 0 
-          ? <Button1 handleClick={() => setRolls(rolls - 1)}/>
-          : <span></span>
-
-        }
-
-        <Position positions={positions[1]}/><Button handleClick={() => setPho(pho + 1)}/>
-        {pho > 0 && <Button1 handleClick={() => setPho(pho - 1)}/>}
-
-        <Position positions={positions[2]}/><Button handleClick={() => setCurry(curry + 1)}/>
-        {curry > 0 && <Button1 handleClick={() => setCurry(curry - 1)}/>}
-
-    </ul>
-  );
-};
-
 const Meme = ({totalQuanti, Summary}) => {
   
   if (totalQuanti <= 3) {
   return(
     <div>
       <h1>Not Hungry?</h1>
-      <img src= {dude} alt ='dude' />
+      <img src={dude} alt ='dude' />
     </div>
     
     
@@ -105,8 +108,8 @@ const Meme = ({totalQuanti, Summary}) => {
     <div>
       <h1>DAMN</h1>
       <img 
-      src = ''
-      alt = 'd' />
+      src = {damn}
+      alt = 'damn' />
     </div>
   )
   }  
